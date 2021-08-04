7d96f1855d67a4b7baf9bc0adda1094c693748d8
Suryashahi07 <87429071+Suryashahi07@users.noreply.github.com>
2021-08-04 12:55:11 +0530

    Add files via upload
    Here are the files for the given task

---
+++ HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OfficalModule2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Button1()
+        {
+            return View();
+        }
+        public ActionResult Button2()
+        {
+            return View();
+        }
+        public ActionResult Button3()
+        {
+            return View();
+        }
+    }
+}
